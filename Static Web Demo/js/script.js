cf3abaaae37325b400795ac5a2b7cca5190ad563
Magic-Doufu <gtrx8fd3ds@gmail.com>
2015-10-29 19:25:48 +0800

    20151029 new bulid
    using scss to output .css file
    完成度約80%
    尚未完工
    圖片有版權問題不要直接上線。

--- Static Web Demo/js/script.js
+++ Static Web Demo/js/script.js
@@ -1,5 +1,13 @@
-$(document).ready(function()) {
-
-  $('html').height($('window').innerHeight());
-
-}
+$(document).ready(function() {
+  $("span[data-pageNum]").click(function() {
+    var pgnum = $(this).attr("data-pageNum");
+    $(".noscrollContent").css({"overflow-x":"scroll"}).animate({scrollLeft:pgnum * $(window).innerWidth()},300,function() { $("span[data-pageNum=" + pgnum + "]").addClass("active"); $("span[data-pageNum!=" + pgnum + "]").removeClass("active"); });
+  });
+  $(".noscroll").scroll(function () {
+    var scrollVal = $(this).scrollTop();
+    console.log(scrollVal)
+  });
+  $(".btn").click(function(){
+    $(".noscroll").animate({scrollTop:$(window).innerHeight()},1000,"swing")
+  });
+})
