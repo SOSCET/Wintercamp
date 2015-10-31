7e90d6b24d5a83fdcee6ee62b19295ac0aa9fb5a
Magic-Doufu <gtrx8fd3ds@gmail.com>
2015-10-31 22:35:31 +0800

    The main part has been done
    please input your data..
    
    sign up page will done in my next commit.
    
    please input the joinusgirl.png.

--- Static Web Demo/js/script.js
+++ Static Web Demo/js/script.js
@@ -1,13 +1,53 @@
 $(document).ready(function() {
+  var scrollVal = 0, scrollOffset = 0, pagenum = 1;
+  var spyarr = [];
+  scrollOffsetBase = $(".noscroll").scrollTop() + $(".content[data-pageNum=1]").offset().top - $("nav").height();
+  scrollOffset = scrollOffsetBase;
+  function scOffset(pgnum) {
+    scrollOffset = $(".noscroll").scrollTop() + $(".content[data-pageNum=" + pgnum + "]").offset().top - $("nav").height();
+    $("span[data-pageNum=" + pgnum + "]").addClass("active");
+    $("span[data-pageNum!=" + pgnum + "]").removeClass("active");
+  }
+  function first() {
+    if (scrollVal >= $(window).innerHeight() * 0.6) {
+      $(".noscrollContent").css({"overflow-x":"scroll","opacity":"1"});
+      $(".noscroll").unbind('scroll',first);
+    }
+  }
+  function base() {
+    scrollVal = $(this).scrollTop();
+    if (scrollVal >= $(window).innerHeight()) {
+      $("nav").css({"position":"fixed","top":"0px"});
+    }else{
+      $("nav").css({"position":"absolute","top":"0px"});
+    }
+  }
+  function spy() {
+    if (scrollVal > scrollOffset + $(".content[data-pageNum=" + pagenum + "]").height() - $("nav").height() && scrollVal > scrollOffsetBase && pagenum < 4) {
+      pagenum += 1;
+      scOffset(pagenum);
+    } else if (scrollVal < scrollOffset - $("nav").height() && scrollVal > scrollOffsetBase) {
+      pagenum -= 1;
+      scOffset(pagenum);
+    } else if (scrollVal < scrollOffsetBase && pagenum != 1) {
+      pagenum = 1;
+      scOffset(pagenum);
+    };
+  }
   $("span[data-pageNum]").click(function() {
-    var pgnum = $(this).attr("data-pageNum");
-    $(".noscrollContent").css({"overflow-x":"scroll"}).animate({scrollLeft:pgnum * $(window).innerWidth()},300,function() { $("span[data-pageNum=" + pgnum + "]").addClass("active"); $("span[data-pageNum!=" + pgnum + "]").removeClass("active"); });
+    $(".noscroll").unbind('scroll',spy);
+    pagenum = parseInt($(this).attr("data-pageNum"));
+    scOffset(pagenum);
+    $(".noscroll").animate({scrollTop:scrollOffset},300,function() {
+      $(".noscroll").bind('scroll',spy);
+    });
   });
-  $(".noscroll").scroll(function () {
     var scrollVal = $(this).scrollTop();
-    console.log(scrollVal)
-  });
-  $(".btn").click(function(){
+    console.log(scrollVal);
+  $(".noscroll").scroll(first);
+  $(".noscroll").scroll(base);
+  $(".noscroll").scroll(spy);
+  $(".btn").click(function() {
     $(".noscroll").animate({scrollTop:$(window).innerHeight()},1000,"swing")
   });
 })
